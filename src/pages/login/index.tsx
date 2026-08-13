6ad4c4d164d6c2feb1161b0074dc913363e91439
williamcwlf-ui <williamcwlf@gmail.com>
2026-08-13 16:55:31 +0100

    Update index.tsx

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -65,7 +65,7 @@ export default function Page() {
 
       <div className="max-w-sm w-full self-center mt-4 justify-center flex flex-col">
         <div className='flex flex-col gap-4'>
-          <Button href={`https://authorize.roblox.com/?client_id=7169779610945884414&response_type=Code&redirect_uri=${encodeURI(`${baseUrl}/auth/roblox`)}&scope=${[
+          <Button href={`https://authorize.roblox.com/?client_id=${process.env?.NEXT_PUBLIC_ROBLOX_CLIENT_ID}&response_type=Code&redirect_uri=${encodeURI(`${baseUrl}/auth/roblox`)}&scope=${[
             'openid',
             'profile',
             'group:read'
