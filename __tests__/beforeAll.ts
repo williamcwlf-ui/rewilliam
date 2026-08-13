e555cb413f7ca2201b108fb62858882cc08b4ef6
williamcwlf-ui <williamcwlf@gmail.com>
2026-08-13 17:27:50 +0100

    Delete __tests__ directory

--- __tests__/beforeAll.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { env } from '~/services/env';
-export default function () {
-    // console.log(env)
-    if (!env.MONGODB_URI) {
-        throw new Error('MONGODB_URI is not set in the environment variables');
-    }
-}
